51b21f65bfde62454dd95f09a413977ae8e98965
VladislavShevchenko <vladshasamurai@gmail.com>
2025-11-29 02:43:32 +0300

    trying route

--- food-app/src/App.tsx
+++ food-app/src/App.tsx
@@ -1,6 +1,25 @@
 import { useState, type MouseEvent } from 'react';
 import Button from './components/Button/Button';
 import Input from './components/Input/Input';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Menu } from './pages/Menu/Menu';
+import { Cart } from './pages/Cart/Cart';
+import { Error } from './pages/Error/Error';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Menu/>
+	},
+	{
+		path: '/cart',
+		element: <Cart/>
+	},
+	{
+		path: '*',
+		element: <Error/>
+	}
+]);
 
 function App() {
 
@@ -15,6 +34,16 @@ function App() {
 			<Button onClick={addCounter}>Кнопка</Button>
 			<Button appearance='big' onClick={addCounter}>Кнопка</Button>
 			<Input placeholder='Email'/>
+			<div>
+				<a href='/'>Меню</a>
+				<a href='/cart'>Корзина</a>
+			</div>
+			<RouterProvider router={router}/>
+			{/* <Routes>
+				<Route path='/' element={<Menu/>}/>
+				<Route path='/cart' element={<Cart/>}/>
+				<Route path='*' element={<Error/>}/>
+			</Routes> */}
 		</>
 	);
 }
