496315fe09847b72b0ba5324e65dc79d8bb03d05
VladislavShevchenko <vladshasamurai@gmail.com>
2025-12-09 13:03:17 +0300

    Some structural changes

--- src/layout/LayoutMenu/Layout.tsx
+++ src/layout/LayoutMenu/Layout.tsx
@@ -1,8 +1,10 @@
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 import styles from './Layout.module.css';
 import Button from '../../components/Button/Button';
+import cn from 'classnames';
 
 export function Layout () {
+
 	return (
 		<div className={styles['layout']}>
 			<div className={styles['side-bar']}>
@@ -12,12 +14,16 @@ export function Layout () {
 					<div className={styles['email']}>email</div>
 				</div>
 				<div className={styles['menu']}>
-					<Link to='/' className={styles['link']}>
+					<NavLink to='/' className={({isActive}) => cn(styles['link'], {
+						[styles.active]: isActive
+					})}>
 						<img src='/menu-icon.svg' alt='menu icon'/>
-					Меню</Link>
-					<Link to='/cart' className={styles['link']}>
+					Меню</NavLink>
+					<NavLink to='/cart' className={({isActive}) => cn(styles['link'], {
+						[styles.active]: isActive
+					})}>
 						<img src='/cart-icon.svg' alt='cart icon'/>
-					Корзина</Link>
+					Корзина</NavLink>
 				</div>
 				<Button className={styles['exit']}>
 					<img src='/exit-icon.svg' alt='exit icon'/>
